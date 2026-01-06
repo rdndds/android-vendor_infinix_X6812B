265ba502d7eb8f3377a035f57a0984118a0236a9
rdndds <ardiandideyashidiq@gmail.com>
2026-01-06 22:24:03 +0700

    init

---
+++ Android.mk
@@ -0,0 +1,19 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),X6812B)
+
+$(call add-radio-file-sha1-checked,radio/gz.img,a0631ba97bdbf608e086695d201636bf6a13930f)
+$(call add-radio-file-sha1-checked,radio/lk.img,0c417635aa3909dc9970283948668ab9cb8f8bdb)
+$(call add-radio-file-sha1-checked,radio/logo.img,21161716f080ebb7766748adc3a5ae67dbcdc7f8)
+$(call add-radio-file-sha1-checked,radio/md1img.img,fa329274d7712f9645742a93d18c0659b6df75ef)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,1295e727b9372a36d225056529444a24e70fc426)
+$(call add-radio-file-sha1-checked,radio/scp.img,df47ac79950947cbabfeadb1a5ae912525112b54)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,8bf7a1e9382c4e797e8033ac6a5197fe1da6d555)
+$(call add-radio-file-sha1-checked,radio/sspm.img,af23c4ce04760ed4e8b3be80f734336d030f5c16)
+$(call add-radio-file-sha1-checked,radio/tee.img,8c2af428bc66e29ddfaefd7dcbd0a2a106fccc72)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    gz \
+    lk \
+    logo \
+    md1img \
+    preloader_raw \
+    scp \
+    spmfw \
+    sspm \
+    tee
